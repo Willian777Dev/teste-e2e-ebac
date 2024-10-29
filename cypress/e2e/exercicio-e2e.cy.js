8f10c14c6c54e88344ddf4d47e06472ea1829b0f
Wilian Koppe <williankoppe70@gmail.com>
2024-10-29 14:29:32 -0300

    Código dando erro

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import produtosPage from "../support/page_objects/produtos.page";
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
@@ -10,13 +12,57 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
       //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
       
   });
+  
+  it('Deve realizar o login com sucesso', () => {
+    cy.get('#username').type('aluno_ebac@teste.com')
+    cy.get('#password').type('teste@teste.com')
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+  });
+
 
 
 })
+
+describe('Funcionalidade= Selecionar produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar 1 produtos da lista', () => {
+        cy.get('.block-inner').first().click()
+        cy.get('.product_title').should('contain', 'Abominable Hoodie')
+    });
+
+    it('Deve selecionar 1 produto da lista', () => {
+        produtosPage.buscarProdutoLista('Argus All-Weather Tank')
+        cy.get('.product_title').should('contain', 'Argus All-Weather Tank')
+    });
+
+    it('Deve selecionar 1 produto da lista', () => {
+        produtosPage.buscarProdutoLista('Ajax Full-Zip Sweatshirt')
+        cy.get('.product_title').should('contain', 'Ajax Full-Zip Sweatshirt')
+    });
+
+    it('Deve buscar 1 produto com sucesso', () => {
+        produtosPage.buscarProduto('Eos V-Neck Hoodie')
+        cy.get('.product_title').should('contain', 'Eos V-Neck Hoodie')
+    });
+});
+
+describe('Funcionalidade= Adicionar produto ao carrinho', () => {
+
+    it('Deve adicionar 1 produto ao carrinho', () => {
+        produtosPage.buscarProdutoLista('Ajax Full-Zip Sweatshirt')
+        produtosPage.addProdutoCarrinho('XS', 'Green', 3)
+    });
+    
+});
---
+++ cypress/support/page_objects/produtos.page.js
@@ -0,0 +1,26 @@
+class ProdutosPage {
+
+    buscarProduto(nomeProduto) {
+        cy.get('[name="s"]').eq(1).type(nomeProduto)
+        cy.get('.button-search').eq(1).click()
+    }
+
+    buscarProdutoLista(nomeProduto) {
+        cy.get('.products > .row')
+        .contains(nomeProduto)
+        .click()
+    }
+
+    visitarProduto() {
+
+    }
+
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get('.button-variable-item-' + cor).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+    }
+}
+
+export default new ProdutosPage()
