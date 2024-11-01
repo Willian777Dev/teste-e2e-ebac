edb0ea1faaa46d27ac9ebf88ef91a3c2c5d1e8c0
Wilian Koppe <williankoppe70@gmail.com>
2024-11-01 10:36:43 -0300

    Teste EBAC E2E

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -19,10 +19,6 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   beforeEach(() => {
     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
   });
-
-  describe('Funcionalidade= Login', () => {
-    
-  });
   
   it('Deve realizar o login com sucesso', () => {
     cy.get('#username').type('aluno_ebac@teste.com')
@@ -31,28 +27,19 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
   });
 
+  describe('Funcionalidade = Fluxo de teste', () => {
 
-
-})
-
-describe('Funcionalidade= Selecionar produtos', () => {
-
-    beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
-    });
-
-    it('Deve selecionar 1 produtos da lista', () => {
-        cy.get('.block-inner').first().click()
-        cy.get('.product_title').should('contain', 'Abominable Hoodie')
-    });
+  beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+  });
 
     it('Deve selecionar 1 produto da lista', () => {
-        produtosPage.buscarProdutoLista('Argus All-Weather Tank')
+        produtosPage.buscarProduto('Argus All-Weather Tank')
         cy.get('.product_title').should('contain', 'Argus All-Weather Tank')
     });
 
     it('Deve selecionar 1 produto da lista', () => {
-        produtosPage.buscarProdutoLista('Ajax Full-Zip Sweatshirt')
+        produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
         cy.get('.product_title').should('contain', 'Ajax Full-Zip Sweatshirt')
     });
 
@@ -60,13 +47,19 @@ describe('Funcionalidade= Selecionar produtos', () => {
         produtosPage.buscarProduto('Eos V-Neck Hoodie')
         cy.get('.product_title').should('contain', 'Eos V-Neck Hoodie')
     });
-});
-
-describe('Funcionalidade= Adicionar produto ao carrinho', () => {
 
     it('Deve adicionar 1 produto ao carrinho', () => {
-        produtosPage.buscarProdutoLista('Ajax Full-Zip Sweatshirt')
+        produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
         produtosPage.addProdutoCarrinho('XS', 'Green', 3)
+        cy.get('.product_title').should('contain', 'Ajax Full-Zip Sweatshirt')
     });
-    
-});
+
+    it('Deve adicionar 1 produto ao carrinho', () => {
+        produtosPage.buscarProduto('Tristan Endurance Tank')
+        produtosPage.addProdutoCarrinho('S', 'Gray', 6)
+        cy.get('.product_title').should('contain', 'Tristan Endurance Tank')
+    });
+
+  })
+
+})
